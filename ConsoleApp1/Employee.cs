ca65388d8ae5838121b45a6fa3d25280bb2f9fa2
Muhammad Saber <msaber@hemmahq.com>
2024-11-20 17:02:55 +0200

    first commit

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,91 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public int id;
+        public string? name;
+        public string? address;
+        public string? phone;
+        public int salary;
+
+
+        public void SetId(int id) { 
+        this.id = id;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public void SetAddress(string address)
+        {
+            this.address = address;
+        }
+
+        public bool SetPhone(string phoneFromUser)
+        {
+            if (phoneFromUser.Length == 11) {
+                if (phoneFromUser.StartsWith("010") || phoneFromUser.StartsWith("011") || phoneFromUser.StartsWith("012") || phoneFromUser.StartsWith("015"))
+                {
+                    this.phone = phoneFromUser;
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("The string does not start with any of the specified prefixes, try again");
+                    return false;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(phoneFromUser.Length);
+                Console.WriteLine("Invalid number, try again");
+                return false;
+            }
+        }
+
+
+        public bool SetSalary(string salary)
+        {
+            if (Convert.ToInt32(salary) >= 6000)
+            {
+                this.salary = Convert.ToInt32(salary);
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid salary, try again");
+                return false;
+            }
+        }
+
+
+        public void IncreaseSalary(string newSalary)
+        {
+            int newSalaryInt = Convert.ToInt32(newSalary);
+            newSalaryInt = Math.Abs(newSalaryInt);
+                this.salary += newSalaryInt;
+                Console.WriteLine($"New Salary is {this.salary}");
+        }
+
+
+        public void Display ()
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Hello {this.name},\n the id of this employee is {this.id},\n salary is {this.salary},\n phone number is {this.phone},\n his address is {this.address}");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            int id;
+            string name, address, phone,salary;
+            bool validPhone = false;
+            bool validSalary = false;
+            bool stableSalary = false;
+
+
+
+
+            Console.WriteLine("Hello, please set Employee name");
+            name = Console.ReadLine() ;
+            Emp1.SetName(name);
+
+            Console.WriteLine("Please set Employee id");
+            id = Convert.ToInt32(Console.ReadLine());
+            Emp1.SetId(id);
+
+            Console.WriteLine("Please set Employee Address");
+            address = Console.ReadLine();
+            Emp1.SetAddress(address);
+
+
+            do
+            {
+                Console.WriteLine("Please set Phone number");
+                phone = Console.ReadLine();
+                validPhone = Emp1.SetPhone(phone);
+            }
+            while (!validPhone);
+
+            do
+            {
+                Console.WriteLine("Please set salary");
+                salary = Console.ReadLine();
+                validSalary = Emp1.SetSalary(salary);
+            }
+            while (!validPhone);
+
+
+
+
+            do
+            {
+                string yesOrNoCondition;
+                Console.WriteLine("Do you wanna increase salary? 'y' to continue..");
+                yesOrNoCondition = Console.ReadLine();
+                if(yesOrNoCondition == "y" || yesOrNoCondition == "Y")
+                {
+                    string increaseSalaryBy;
+                    Console.WriteLine("Please enter increasing value..");
+                    increaseSalaryBy = Console.ReadLine();
+                    Emp1.IncreaseSalary(increaseSalaryBy);
+                } else
+                {
+                    stableSalary = true;
+                }
+               
+            }
+            while (!stableSalary);
+
+
+
+            Emp1.Display();
+
+
+
+
+
+
+        }
+    }
+}
